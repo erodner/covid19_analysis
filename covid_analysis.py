8ff550aa17a4d2f3c0d3a035de31a4c4a47520e0
Erik Rodner <Erik.Rodner@htw-berlin.de>
2020-12-02 22:34:17 +0100

    example

--- covid_analysis.py
+++ covid_analysis.py
@@ -1,5 +1,25 @@
 import rki
+import matplotlib.pylab as plt
 
-dates, cases = rki.get_positive_cases()
+dates, cases = rki.get_cases_last7days()
 
-print (cases)
+for i in range(1, len(dates)):
+    change_date = dates[i]
+    delta = cases[i]-cases[i-1]
+    if delta<0:
+        print (f"Rückgang am {change_date}")
+    
+    if delta>0:
+        print (f"Anstieg am {change_date}")
+    
+
+dates, cases_berlin = rki.get_cases_last7days("Berlin")
+
+alphas = []
+for i in range(len(dates)):
+    change_date = dates[i]
+    alphas.append(cases_berlin[i]/cases[i])
+
+plt.figure()
+plt.plot(dates, alphas)
+plt.show()
